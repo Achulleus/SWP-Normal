a6debc8511d49cda244733c826b57c95f6202758
Achilleus <56840977+Achulleus@users.noreply.github.com>
2021-01-26 17:03:04 +0100

    Add files via upload

--- AktienAusfuehrung.java
+++ AktienAusfuehrung.java
@@ -1,12 +1,13 @@
 package aktien;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -21,9 +22,15 @@ import java.util.Scanner;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import org.apache.commons.io.IOUtils;
@@ -32,21 +39,21 @@ import org.json.JSONObject;
 
 public class AktienAusfuehrung {
 	
-	private static String aktie;
-	private static String URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + aktie + "&outputsize=full&apikey=GV8OZAQLF4YSTSGD";
-	private static int tage = 100;
-	private static ArrayList<LocalDate> daten = new ArrayList<LocalDate>(); 
-	private static ArrayList<Double> closeWerte = new ArrayList<Double>();
-	private static double [] d200Schnitt = new double[tage + 200];
+	public static String aktie;
+	public static String URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + aktie + "&outputsize=full&apikey=GV8OZAQLF4YSTSGD";
+	public static ArrayList<LocalDate> daten = new ArrayList<LocalDate>(); 
+	public static ArrayList<Double> closeWerte = new ArrayList<Double>();
+	public static ArrayList<Double> d200Schnitt = new ArrayList<Double>();
 	
 	final static String hostname = "localhost";
 	final static String port = "3306";
-	final static String db = "Aktien";
+	final static String db = "aktien";
 	final static String user = "root";
 	final static String password = "1234";
-
+	
 	public static void main(String[] args){
 		eingabe();
+		AktienFenster a = new AktienFenster(aktie);
 		
 		try {
 			getWert(URL);
@@ -58,20 +65,21 @@ public class AktienAusfuehrung {
 		
 		tabelleErstellen();
 		datenbankeingabe();
-		datenbankausgabe();
+		//datenbankausgabe();
 	}
 	
 	public static void eingabe() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Welche Aktie wollen Sie dargestellt?");
+		System.out.println("Welchen Aktienkurs wollen Sie dargestellt haben?");
 		aktie = sc.next();
+		URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + aktie + "&outputsize=full&apikey=GV8OZAQLF4YSTSGD";
 	}
 	
 	public static void getWert(String URL) throws JSONException, IOException {
 		try {
 			JSONObject json = new JSONObject(IOUtils.toString(new URL(URL), Charset.forName("UTF-8")));
 			json = json.getJSONObject("Time Series (Daily)");
-			for(int i = 0; i < tage; i++){
+			for(int i = 0; i < json.length(); i++){
 				daten.add(LocalDate.parse((CharSequence)json.names().get(i)));
 				closeWerte.add(json.getJSONObject(LocalDate.parse((CharSequence)json.names().get(i)).toString()).getDouble("4. close"));
 			}
@@ -80,10 +88,9 @@ public class AktienAusfuehrung {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 	
-	public static void d200SchnittBerechnen() {
+	/*public static void d200SchnittBerechnen() {
 		for(int i = 0; i < d200Schnitt.length; i++) {
 			int sum = 0;
 			for(int j = 0; j < 200; j++) {
@@ -92,7 +99,7 @@ public class AktienAusfuehrung {
 			d200Schnitt[i+200] = sum/200;
 		}
 		datenbankUpdate200Schnitt();
-	}
+	}*/
 	
 	public static void tabelleErstellen() {
 		Connection con = null;
@@ -107,8 +114,13 @@ public class AktienAusfuehrung {
 		try {
             con = DriverManager.getConnection("jdbc:mysql://"+hostname+"/"+db+"?user="+user+"&password="+password+"&serverTimezone=UTC");
             Statement myStat = con.createStatement();
-            String sql = "CREATE TABLE "+ aktie +" if not exists(Datum dateTime, closeWert doubble, 200-Schnitt doubble))";
+            String sql = "CREATE TABLE "+ aktie +" if not exists(Datum dateTime not null Primary Key, closeWert double)";
             myStat.execute(sql);
+            
+            Statement myStatHS = con.createStatement();
+            String sqlHS = "CREATE TABLE 200Schnitt " + aktie + " if not exists(Datum dateTime not null Primary Key, 200Schnitt double)";
+            myStat.execute(sqlHS);
+            
             con.close();
         }
         catch (SQLException sqle) {
@@ -131,11 +143,24 @@ public class AktienAusfuehrung {
         try {
             con = DriverManager.getConnection("jdbc:mysql://"+hostname+"/"+db+"?user="+user+"&password="+password+"&serverTimezone=UTC");
             Statement myStat = con.createStatement();
-            for(int i = 0; i < tage; i++) {
-            	String sql = "INSERT INTO "+ aktie +" values("+ daten.get(i) +","+ closeWerte.get(i) +","+ d200Schnitt[i] +")";
-            	myStat.execute(sql);
+            
+            for(int i = 0; i < daten.size(); i++) {
+            	PreparedStatement pstatement = con.prepareStatement("INSERT INTO "+ aktie +" values( ?, ?)");
+                pstatement.setDate(1, Date.valueOf(daten.get(i)));
+                pstatement.setDouble(2, closeWerte.get(i));
+                pstatement.executeUpdate();  
             }
+            
             ordnen(con);
+            d200SchnittBerechnen(con);
+            
+            for(int i = 0; i < daten.size(); i++) {
+            	PreparedStatement pstatement = con.prepareStatement("INSERT INTO 200Schnitt " + aktie + " values( ?, ?)");
+                pstatement.setDate(1, Date.valueOf(daten.get(i)));
+                pstatement.setDouble(2, d200Schnitt.get(i));
+                pstatement.executeUpdate();  
+            }
+            
             con.close();
         }
         catch (SQLException sqle) {
@@ -147,7 +172,7 @@ public class AktienAusfuehrung {
 	}
 	
 	public static void ordnen(Connection connection) {
-        String selectFrom = "SELECT * FROM "+aktie+" ORDER BY DATUM ASC";
+        String selectFrom = "SELECT * FROM "+aktie+" ORDER BY DATUM desc";
 
         try {
             Statement stmt = connection.createStatement();
@@ -164,10 +189,27 @@ public class AktienAusfuehrung {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        d200SchnittBerechnen();
+	}
+     
+	public static void d200SchnittBerechnen(Connection connection) {
+		d200Schnitt.clear();
+		
+		try {
+        for(int i = 0; i < daten.size() - 200; i++) {
+            	PreparedStatement pstatement = connection.prepareStatement("with temp as (" + 
+            		"    select closeWert from " + aktie + " where day <= ? order by Datum desc limit 200)" + 
+            		"    select avg(closeWert) from temp"); 
+				pstatement.setDate(1, Date.valueOf(daten.get(i)));
+				ResultSet rs = pstatement.executeQuery();
+				rs.next();
+				d200Schnitt.add(rs.getDouble(1));
+        }
+        } catch (SQLException e) {
+			e.printStackTrace();
+		} 
     }
 	
-	public static void datenbankUpdate200Schnitt() {
+	/*public static void datenbankUpdate200Schnitt() {
 		Connection con = null;
 
         try {
@@ -180,7 +222,7 @@ public class AktienAusfuehrung {
             con = DriverManager.getConnection("jdbc:mysql://"+hostname+"/"+db+"?user="+user+"&password="+password+"&serverTimezone=UTC");
             Statement myStat = con.createStatement();
             for(int i = 0; i < tage; i++) {
-            	String sql = "UPDATE "+ aktie +" SET 200-Schnitt="+ d200Schnitt[i] +" WHERE Datum="+ daten.get(i);
+            	String sql = "UPDATE "+ aktie +" SET 200-Schnitt="+ d200Schnitt.get(i) +" WHERE Datum="+ daten.get(i);
             	myStat.execute(sql);
             }
             con.close();
@@ -191,9 +233,9 @@ public class AktienAusfuehrung {
             System.out.println("Error: " + sqle.getErrorCode());
             sqle.printStackTrace();
         }
-	}
+	}*/
 	
-	private static void datenbankausgabe(){
+	/*private static void datenbankausgabe(){
         Connection con = null;
 
         try {
@@ -225,6 +267,6 @@ public class AktienAusfuehrung {
             System.out.println("Error: " + sqle.getErrorCode());
             sqle.printStackTrace();
         }
-    }
+    }*/
 }
 
---
+++ AktienFenster.java
@@ -0,0 +1,64 @@
+package aktien;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.time.LocalDate;
+
+import javax.swing.JFrame;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.AbstractRenderer;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+
+public class AktienFenster {
+	
+	JFrame jframe;
+	ChartPanel chartpanel;
+	
+	public AktienFenster(String fenstername) {
+		this.jframe = new JFrame(fenstername);
+		this.jframe.setSize(700, 500);
+		this.chartpanel = new ChartPanel(chartErstellen(fenstername));
+		this.jframe.setVisible(true);
+	}
+	
+	public JFreeChart chartErstellen(String fenstername) {
+		TimeSeries werte = new TimeSeries("closeWerte");
+		for(int i = 0; i < AktienAusfuehrung.daten.size(); i++) {
+			LocalDate datum = AktienAusfuehrung.daten.get(i);
+			double wert = AktienAusfuehrung.closeWerte.get(i);
+			int tag = datum.getDayOfMonth();
+			int monat = datum.getMonthValue();
+			int jahr = datum.getYear();
+			werte.add(new Day(tag,monat,jahr), wert);
+		}
+		
+		TimeSeries d200werte = new TimeSeries("200Schnitt");
+		for(int i = 0; i < AktienAusfuehrung.d200Schnitt.size(); i++) {
+			LocalDate datum = AktienAusfuehrung.daten.get(i);
+			double d200wert = AktienAusfuehrung.d200Schnitt.get(i);
+			int tag = datum.getDayOfMonth();
+			int monat = datum.getMonthValue();
+			int jahr = datum.getYear();
+			d200werte.add(new Day(tag,monat,jahr), d200wert);
+		}
+		
+		TimeSeriesCollection collection = new TimeSeriesCollection();
+		collection.addSeries(werte);
+		collection.addSeries(d200werte);
+		
+		JFreeChart chart = ChartFactory.createTimeSeriesChart(fenstername, "Datum", "CloseWert", collection);
+		
+		XYPlot plot = (XYPlot) chart.getPlot();
+        plot.getRenderer().setDefaultStroke(new BasicStroke(2.0f));
+        ((AbstractRenderer) plot.getRenderer()).setAutoPopulateSeriesStroke(false);
+        ((AbstractRenderer) plot.getRenderer()).setSeriesPaint(0,  new Color(0, 0, 0));
+        
+        return chart;
+	}
+}
